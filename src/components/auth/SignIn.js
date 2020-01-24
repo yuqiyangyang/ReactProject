c166e590242e7cc8da599bffaf7d7b44b48356c2
yuqi <yuqi.yang@mail.mcgill.ca>
2020-01-24 17:02:57 -0500

    conenct signIn with redux

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
     state ={
@@ -14,7 +16,7 @@ class SignIn extends Component {
 
     handleSubmit =(e) =>{
         e.preventDefault();
-        console.log(this.state);
+        this.props.signIn(this.state)
 }
 
     render() {
@@ -39,4 +41,11 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signIn:(creds) => dispatch(signIn(creds))
+    }
+}
+
+
+export default connect(null, mapDispatchToProps)(SignIn)
