f4350f99043c8a9fab44170f6cf308c84aeaf1ea
yuqi <yuqi.yang@mail.mcgill.ca>
2020-01-24 17:36:51 -0500

    connect signIn to firebase and redux

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { signIn } from '../../store/actions/authActions'
+import { signIn } from '../../store/actions/authActions'//import action creator signiN
 
 class SignIn extends Component {
     state ={
@@ -16,10 +16,11 @@ class SignIn extends Component {
 
     handleSubmit =(e) =>{
         e.preventDefault();
-        this.props.signIn(this.state)
+        this.props.signIn(this.state)// THAT is the credential
 }
 
     render() {
+        const { authError} = this.props;
         return (
             <div className="container">
                 <form onSubmit={this.handleSubmit} className="white">
@@ -34,10 +35,19 @@ class SignIn extends Component {
                     </div>
                     <div className="input-field">
                         <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    <div className = "red-text center">
+                        { authError? <p>{authError}</p>: null}
+                    </div>
                     </div>
                 </form>     
             </div>
         )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError
     }
 }
 
@@ -48,4 +58,4 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect(null, mapDispatchToProps)(SignIn)
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -6,11 +6,13 @@ const initState = {
 const authReducer = (state = initState, action) => {
   switch(action.type){
     case 'LOGIN_ERROR':
+      console.log('login error');
     return {
       ...state, 
       authError: 'Login Failed'
     }
     case 'LOGIN_SUCCESS':
+      console.log('login success');
       return{
         ...state,
       authError: null
