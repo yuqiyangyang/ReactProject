f939402a47cf22dfc101555cbe787134ab7f903f
yuqi <yuqi.yang@mail.mcgill.ca>
2020-01-25 16:23:59 -0500

    correct yuqi mistake

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
   
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCsYO3THsV6tBIgjN6robJmJCZCBA-Twg8",
-    authDomain: "marioplan-c222e.firebaseapp.com",
-    databaseURL: "https://marioplan-c222e.firebaseio.com",
-    projectId: "marioplan-c222e",
-    storageBucket: "marioplan-c222e.appspot.com",
-    messagingSenderId: "868117437813",
-    appId: "1:868117437813:web:9b0ab084ec0d7b031a073c",
-    measurementId: "G-PEV938B21N"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_MEASUREMENT_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -22,3 +22,27 @@ export const signOut = () => {//this is a function
     }
 }
 
+export const signUp = (newUser) =>{
+    //need to store user info to user collection in firestore
+    return (dispatch, getState, {getFirebase, getFirestore}) =>{
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        //use this to generate new users
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password// will communicate with firebase, asychonise 
+        ).then((resp) =>{
+            return firestore.collection('users').doc(resp.user.uid).set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0]+newUser.lastName[0]//better to store this info 
+            }).then(()=>{
+                dispatch({type: 'SIGNUP_SUCCESS'})
+            })
+        }).catch(err=>{
+            dispatch({type: 'SIGNUP_ERROR', err})
+            
+        })
+    }
+}
