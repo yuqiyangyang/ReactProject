b4147e6e5203fe87c1eb47a1dc278b817e985557
yuqi <yuqi.yang@mail.mcgill.ca>
2020-01-30 15:11:57 -0500

    use moment method to format dates

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import { compose} from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
   const { project, auth } = props;
@@ -17,8 +18,8 @@ const ProjectDetails = (props) => {
           <p>{project.content}</p>
         </div>
         <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by {project.authorFirstNmae} {project.authorLastName}</div>
-          <div>2nd September, 2am</div>
+          <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
+          <div>{moment(project.createdAt.toDate()).calendar()}</div>
         </div>
       </div>
     </div>)
