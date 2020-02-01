91f9a81905be11e7935ee9183ef5fed7a316ff7b
yuqi <yuqi.yang@mail.mcgill.ca>
2020-01-31 20:17:42 -0500

    final deploy firebase

---
+++ dist/precache-manifest.82aff9c2de02a3639b16328e0fa134e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9149eb799319f60bfbfe563821b999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "051c6e4c0fafd035f79a",
+    "url": "/static/css/main.bae125d1.chunk.css"
+  },
+  {
+    "revision": "a0909cb1f02adca49817",
+    "url": "/static/js/2.f40935b3.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.f40935b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "051c6e4c0fafd035f79a",
+    "url": "/static/js/main.89c3eaff.chunk.js"
+  },
+  {
+    "revision": "e67c4f91b224f78fe18a",
+    "url": "/static/js/runtime-main.6fab081a.js"
+  }
+]);
